4fd47a0bc7c4a924b44219e728257e8aafd96ab2
SeonUI <ksw135468@gmail.com>
2023-03-09 20:00:10 +0900

    adsda

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
     a = b = 0
 
     for i in range(0,len(nums)):
-        for j in range(i,len(nums)):
+        for j in range(i+1,len(nums)):
            if (nums[i]+nums[j] == target):
                return (i,j)
     return (0,0)
